31d7c977774569c4cdc94733502c919172b5a890
Cameron Choi <cameron.choi99@gmail.com>
2021-10-28 22:41:49 +1100

    datasets

--- mmdet/core/post_processing/__init__.py
+++ mmdet/core/post_processing/__init__.py
@@ -3,8 +3,11 @@ from .bbox_nms import fast_nms, multiclass_nms
 from .matrix_nms import mask_matrix_nms
 from .merge_augs import (merge_aug_bboxes, merge_aug_masks,
                          merge_aug_proposals, merge_aug_scores)
+from .rbbox_nms import multiclass_rnms, aug_multiclass_rnms
+from .rmerge_augs import merge_tiles_aug_rbboxes
 
 __all__ = [
     'multiclass_nms', 'merge_aug_proposals', 'merge_aug_bboxes',
-    'merge_aug_scores', 'merge_aug_masks', 'mask_matrix_nms', 'fast_nms'
+    'merge_aug_scores', 'merge_aug_masks', 'mask_matrix_nms', 'fast_nms',
+    'merge_tiles_aug_rbboxes', 'multiclass_rnms', 'aug_multiclass_rnms'
 ]
