cb5703dd614be12d4ce1925997c945a4efdee867
Cameron Choi <cameron.choi99@gmail.com>
2021-10-28 22:55:09 +1100

    Finish

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .collect_env import collect_env
 from .logger import get_root_logger
+from .rbbox_visualization import imshow_det_rbboxes
 
-__all__ = ['get_root_logger', 'collect_env']
+__all__ = ['get_root_logger', 'collect_env', 'imshow_det_rbboxes']
